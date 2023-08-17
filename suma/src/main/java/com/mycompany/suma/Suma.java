2f530f61826e891420e22113aaa8dd50af98ede9
samuel-soto2 <1461888@senati.pe>
2023-08-17 11:03:01 -0500

    first commit

---
+++ suma/src/main/java/com/mycompany/suma/Suma.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.suma;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Suma {
+
+    public static void main(String[] args) {
+        int numero_1;
+        int numero_2;
+        int resultado;
+        
+        Scanner constructor=new Scanner(System.in);
+        System.out.println("Por favor ingrese el primer número: ");
+        numero_1=constructor.nextInt();
+        
+      
+        System.out.println("Por favor ingrese el segundo número: ");
+        numero_2=constructor.nextInt();
+        
+        resultado=numero_1 + numero_2;
+        
+        System.out.println("El resultado de la suma es:"+resultado);
+        
+           
+        
+    }
+}
