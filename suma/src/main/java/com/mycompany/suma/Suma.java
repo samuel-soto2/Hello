ae8b8759de60bf725cadd2f077a1d9fb48851e46
samuel-soto2 <1461888@senati.pe>
2023-08-17 11:09:39 -0500

    Se declaran tres variables de tipo entero numero_1, numero_2 y resultado

--- suma/src/main/java/com/mycompany/suma/Suma.java
+++ suma/src/main/java/com/mycompany/suma/Suma.java
@@ -17,17 +17,7 @@ public class Suma {
         int numero_2;
         int resultado;
         
-        Scanner constructor=new Scanner(System.in);
-        System.out.println("Por favor ingrese el primer número: ");
-        numero_1=constructor.nextInt();
         
-      
-        System.out.println("Por favor ingrese el segundo número: ");
-        numero_2=constructor.nextInt();
-        
-        resultado=numero_1 + numero_2;
-        
-        System.out.println("El resultado de la suma es:"+resultado);
         
            
         
